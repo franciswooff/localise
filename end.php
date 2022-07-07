f7c7a32fd93848ac94f9940bb10183aa10c470a2
FrancisW <65406277+franciswooff@users.noreply.github.com>
2022-07-07 17:25:47 +0100

    Update end.php
    Updated to match new feature set

--- end.php
+++ end.php
@@ -5,27 +5,24 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="main.css">
-<link rel="stylesheet" type="text/css" href="page.css">
-<script src="vidaud.js" defer></script>
 </head>
 
 <body>
 <main>
-
 <h1>Localise test end</h1>
 <p>Thanks for taking part in the test. Your results have been submitted.</p>
 
 <?php
 $editme = fopen("EDITME.txt", "r") or die('<p>Unable to open your EDITME.txt file</p>');
-$vidnum=fgets($editme);
+$pagenum=fgets($editme);
 $mail=fgets($editme);
 fclose($editme);
 
 session_start();
 $subno = $_SESSION["subno"];
 
-for ($evc = 1; $evc <= $vidnum; $evc++){
-  $res = $_SESSION["r".$evc];
+for ($i = 1; $i <= $pagenum; $i++){
+  $res = $_SESSION['d'.$i];
   $allres .= "\r\n".$res;
 }
 
