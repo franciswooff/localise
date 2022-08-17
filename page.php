62f43f0d7a17cf9e16cf69857f98d9813807b70e
FrancisW <65406277+franciswooff@users.noreply.github.com>
2022-08-17 09:19:21 +0100

    Update page.php
    Minor updates to align more closely to "vidaudio" project (repo)

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php
-$editme = fopen('EDITME.txt', 'r') or die('<p>Unable to open your EDITME.txt file</p>');
+$editme = fopen('EDITME.txt', 'r') or die('<p>Unable to open EDITME.txt file</p>');
 $numpages = fgets($editme);
 fgets($editme);
 $rndon = fgets($editme);
@@ -7,8 +7,33 @@ $angles = fgets($editme);
 $autoply = fgets($editme);
 $loops = fgets($editme);
 fclose($editme);
+
 session_set_cookie_params(3000,'/');
 session_start();
+
+$cntr = $_SESSION['pagecount'] ?? 0;
+
+if ($cntr == 0) {
+  $pageary = range(1,$numpages );
+  $_SESSION['pagearray'] = $pageary;
+  $_SESSION['subno'] = 'INTRO PAGE SKIPPED';
+}
+
+if (isset($_POST['page'])) {
+  $pageary = $_SESSION['pagearray'];
+  $res = $_POST['degs'];
+  if(is_numeric($res)){
+    $_SESSION['d'.$pageary[$cntr-1]] = $pageary[$cntr-1].' , '.$res.' , '.$cntr;
+  } else {
+    exit('<h1>Something nasty here, try the test again</h1>');
+  }
+}
+
+if ($cntr >= $numpages){
+  header('location:end.php');
+  exit();
+}
+
 if (isset($_POST['start'])) {
   $ptno = $_POST['partno'];
   if(is_numeric($ptno)){
@@ -19,30 +44,12 @@ if (isset($_POST['start'])) {
     exit('<h1>Something nasty here, go back &amp; try again</h1>');
   }
 }
-$cntr = $_SESSION['pagecount'] ?? 0;
-if ($cntr == 0) {
-  $pageary = range(1,$numpages );
-  $_SESSION['pagearray'] = $pageary;
-}
-if (isset($_POST['page'])) {
-  $pageary = $_SESSION['pagearray'];
-  $res = $_POST['degs'];
-  if(is_numeric($res)){
-    $_SESSION['d'.$pageary[$cntr-1]] = $pageary[$cntr-1].'_'.$res;
-  } else {
-    exit('<h1>Something nasty here, try the test again</h1>');
-  }
-}
-$cntr ++;
-$_SESSION['pagecount'] = $cntr;
-if ($cntr > $numpages){
-  header('location:end.php');
-  exit(0);
-}
-if ($rndon > chr(32) && $cntr == 1) {
+
+if ($rndon > chr(32) && $cntr == 0) {
   shuffle($pageary);
   $_SESSION['pagearray'] = $pageary;
 }
+
 if ($angles > chr(32)) {
   $image='head.png';
 } else {
@@ -58,6 +65,7 @@ if ($loops > chr(32)) {
 } else {
   $lp='';
 }
+
 echo '<!doctype html>
 <html lang="en">
 <head>
@@ -69,11 +77,19 @@ echo '<!doctype html>
 <script src="head.js" defer></script>
 </head>
 <body>
-<h1>Localise test '.$cntr.'</h1>
-<audio '.$ap.' '.$lp.'><source src="audiofiles/'.$pageary[$cntr-1].'.wav"></audio>
+
+<h1>Localise test '.($cntr+1).'</h1>
+
+<audio '.$ap.' '.$lp.' src="audiofiles/'.$pageary[$cntr].'.wav"></audio>
+
 <img src="images/'.$image.'" alt="overhead diagram of head showing angles on surrounding circle">
+
 ';
+
+$cntr ++;
+$_SESSION['pagecount'] = $cntr;
 ?>
+
 <form action="page.php" method="post">
   <div class="centr">
     <img src="images/ply.png" alt="play icon"><img src="images/pse.png" alt="pause icon">
@@ -83,5 +99,6 @@ echo '<!doctype html>
     <input type="submit" value="Submit" name="page">
   </div>
 </form>
+
 </body>
 </html>
